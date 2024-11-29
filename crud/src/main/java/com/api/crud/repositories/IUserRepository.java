c31f8765f91bd1192b43af8acc483504a419f74c
Aitor Doncel <adoncel@mpm.es>
2024-11-29 12:49:47 +0100

    Adjusted repo interface ffor database queries

--- crud/src/main/java/com/api/crud/repositories/IUserRepository.java
+++ crud/src/main/java/com/api/crud/repositories/IUserRepository.java
@@ -1,4 +1,11 @@
 package com.api.crud.repositories;
 
-public interface IUserRepository {
+import com.api.crud.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
+
+
 }
