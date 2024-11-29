2fbd807095cd51f60096d771e47136a1d03d3441
Aitor Doncel <adoncel@mpm.es>
2024-11-29 12:46:07 +0100

    Adding controllers

--- crud/src/main/java/com/api/crud/controllers/UserController.java
+++ crud/src/main/java/com/api/crud/controllers/UserController.java
@@ -1,4 +1,50 @@
 package com.api.crud.controllers;
 
+
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserService;
+import jakarta.persistence.Id;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Long id){
+        return this.userService.getByID(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public UserModel updateUserById(@RequestBody UserModel request, @PathVariable("id") Long id){
+        return this.userService.updateById(request,id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteUserById(@PathVariable("id") Long id){
+        boolean ok = this.userService.deleteUser(id);
+        if (ok){
+            return "User with id " + id + " deleted.";
+        } else{
+            return "Error deleting user with id " + id + ".";
+        }
+    }
 }
